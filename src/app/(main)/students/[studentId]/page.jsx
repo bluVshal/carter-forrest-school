6be36691fe083da0246f82a871a453f5ec04e9d3
Vishal <blu.vishal@gmail.com>
2025-03-17 19:57:19 +0400

    Completed implementation of go to top scaffold

--- src/app/(main)/students/[studentId]/page.jsx
+++ src/app/(main)/students/[studentId]/page.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+'use client';
+
+import React, { useRef, useState } from 'react'
+import GoTop from '@/components/GoTop'
 
 const Student = () => {
+  const refScrollUp = useRef();
+  const [showGoTop, setshowGoTop] = useState("goTopHidden");
+
+  //SCROLL UP HANDLER
+  const handleScrollUp = () => {
+    refScrollUp.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div>Specific Student Page</div>
+    <div>
+      Specific Student Page
+      <div ref={refScrollUp}> </div>
+      <GoTop showGoTop={showGoTop} scrollUp={handleScrollUp} />
+    </div>
   )
 }
 
--- src/components/GoTop.js
+++ src/components/GoTop.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { Button } from './ui/button';
 
 const GoTop = (props) => {
+    const [scrollPosition, setSrollPosition] = useState(0);
+
+    const [ showGoTop, setShowGoTop ] = useState( false )
+
+    const handleVisibleButton = () => {
+        setShowGoTop( window.pageYOffset > 50 )
+    }
+    
+    const handleScrollUp = () => {
+        window.scrollTo( { left: 0, top: 0, behavior: 'smooth' } )
+    }
+    //SCROLL LISTENER
+    useEffect(() => {
+        const e = window.addEventListener("scroll", handleVisibleButton);
+        return () => window.removeEventListener('scroll', handleVisibleButton);
+    });
+
+
     return (
         <>
-            <div>
-                <Button className="goTop" onClick={props.scrollUp}>                
-                  <FontAwesomeIcon icon={faCircleChevronUp} />
+            <div className={ showGoTop ? '' : 'goTopHidden' } onClick={ handleScrollUp }>
+                <Button className="goTop" onClick={props.scrollUp}>
+                    <FontAwesomeIcon icon={faCircleChevronUp} />
                 </Button>
             </div>
         </>
