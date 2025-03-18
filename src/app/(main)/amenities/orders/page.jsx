23e1f156558671eaa869acc743d2550a55f7ed5c
Vishal <blu.vishal@gmail.com>
2025-03-18 11:47:50 +0400

    Added fonts for the application

--- src/app/(main)/amenities/orders/page.jsx
+++ src/app/(main)/amenities/orders/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AmenityOrders = () => {
   return (
-    <div>AmenityOrders</div>
+    <div>
+        <div className="font-LS">Amenity Orders</div>
+        <div className="font-FT"> All Amenity orders </div>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
       <div className="ml-3 mt-4 max-h-20">
-        <h1 className="text-4xl">Carter Forrest School</h1>
+        <h1 className="text-4xl font-CT">Carter Forrest School</h1>
       </div>
     </div>
   );
