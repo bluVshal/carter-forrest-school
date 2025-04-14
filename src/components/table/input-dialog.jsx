c3dffc624b2e36a3752262b8e8ea711c3489ad01
Vishal <blu.vishal@gmail.com>
2025-04-14 13:31:02 +0400

    Continued implementation of dialog for display data to save from csv file

--- src/components/table/input-dialog.jsx
+++ src/components/table/input-dialog.jsx
@@ -1,14 +1,41 @@
-import React, { useState }  from 'react'
+import React, { useRef, useState }  from 'react'
 import { Dialog } from 'primereact/dialog';
+import { DataTable } from 'primereact/datatable';
+import { useTranslation } from 'react-i18next';
+import { Column } from 'primereact/column';
 
 const InputDialog = (props) => {
     
     let { visible, csvOutput } = props; 
+    const dt = useRef(null);
+    const [t, i18n] = useTranslation("global");
+    const [selectedData, setSelectedData] = useState(null);
+    const [columns, setColumns] = useState([]);
 
     return (
         <div>
-            <Dialog header="Header" visible={visible} style={{ width: '50vw' }} onHide={() => { if (!visible) return; visible = false; }}>
-                <p className="m-0"> { csvOutput }</p>
+            <Dialog header="Header" visible={visible} style={{ backgroundColor:'white' ,width: '50vw' }} onHide={() => { if (!visible) return; visible = false; }}>
+                <DataTable
+                    ref={dt}
+                    value={csvOutput}
+                    dataKey="id"
+                    sortMode="multiple"
+                    paginator
+                    paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                    currentPageReportTemplate={t('dataTable.totalNumOfRecords') + ": {totalRecords}"}
+                    rows={5}
+                    rowsPerPageOptions={[5, 10, 25, 50, 100]}
+                    tableStyle={{ minWidth: '50rem' }}
+                    selectionMode="single"
+                    selection={selectedData}
+                    onSelectionChange={(e) => setSelectedData(e.value)}
+                    emptyMessage={t('dataTable.noRecordsFound')}
+                >
+                    <Column selectionMode="single" showGridlines stripedRows headerStyle={{ width: '3rem' }}></Column>
+                    {columns.map((col, i) => (
+                        <Column key={col.id} field={col.id} sortable header={t('dataTable.' + col.id)} filter filterField={col.id} filterPlaceholder="Search" />
+                    ))}
+                </DataTable>
             </Dialog>
         </div>
     )
