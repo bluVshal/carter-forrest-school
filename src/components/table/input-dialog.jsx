0d8e976843c99262d977bea1f82ce9cc865d5cfd
Vishal <blu.vishal@gmail.com>
2025-04-15 13:08:22 +0400

    Added button to delete unnecessary data in popup DT

--- src/components/table/input-dialog.jsx
+++ src/components/table/input-dialog.jsx
@@ -1,20 +1,30 @@
-import React, { useRef, useState }  from 'react'
+import React, { useRef, useState } from 'react'
 import { Dialog } from 'primereact/dialog';
 import { DataTable } from 'primereact/datatable';
 import { useTranslation } from 'react-i18next';
 import { Column } from 'primereact/column';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const InputDialog = (props) => {
-    
-    let { visible, csvOutput } = props; 
+
+    let { visible, csvOutput } = props;
     const dt = useRef(null);
     const [t, i18n] = useTranslation("global");
     const [selectedData, setSelectedData] = useState(null);
     const [columns, setColumns] = useState([]);
 
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                <Button onClick={() => confirmDelete(rowData, type)}><FontAwesomeIcon icon={faTrash} /></Button>
+            </React.Fragment>
+        );
+    };
+    
     return (
         <div>
-            <Dialog header="Header" visible={visible} style={{ backgroundColor:'white' ,width: '50vw' }} onHide={() => { if (!visible) return; visible = false; }}>
+            <Dialog header="Header" visible={visible} style={{ backgroundColor: 'white', width: '50vw' }} onHide={() => { if (!visible) return; visible = false; }}>
                 <DataTable
                     ref={dt}
                     value={csvOutput}
@@ -35,6 +45,8 @@ const InputDialog = (props) => {
                     {columns.map((col, i) => (
                         <Column key={col.id} field={col.id} sortable header={t('dataTable.' + col.id)} filter filterField={col.id} filterPlaceholder="Search" />
                     ))}
+
+                    <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '12rem' }}></Column>
                 </DataTable>
             </Dialog>
         </div>
